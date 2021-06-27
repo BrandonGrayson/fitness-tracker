f0d8e67ea8491b04d9934bd2366bf32a121211d7
Brandon <brandonbgrayson@gmail.com>
2021-06-27 19:04:52 -0400

    created a material.module folder to manage all the imports and exports coming from material package

---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    // imports array
+    imports: [],
+    exports: []
+})
+// a class to manage material components
+export class MaterialModule {
+
+}
