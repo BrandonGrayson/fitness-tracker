6cf031d3dc8db5e00150937eca49c21e342d7859
Brandon <brandonbgrayson@gmail.com>
2021-06-27 19:10:54 -0400

    successfully called the MatSliderModule from material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
     // imports array
-    imports: [],
-    exports: []
+    imports: [ MatSliderModule ],
+    exports: [ MatSliderModule ]
 })
 // a class to manage material components
 export class MaterialModule {
