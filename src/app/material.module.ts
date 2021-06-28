efc0d7f03055d26efd0f244492e19741a2b413f3
Brandon <brandonbgrayson@gmail.com>
2021-06-27 20:41:50 -0400

    added a mat raised button with color primary to app component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     // imports array
-    imports: [ MatSliderModule ],
-    exports: [ MatSliderModule ]
+    imports: [ MatSliderModule, MatButtonModule ],
+    exports: [ MatSliderModule, MatButtonModule ]
 })
 // a class to manage material components
 export class MaterialModule {
