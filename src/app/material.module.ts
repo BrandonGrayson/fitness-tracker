729bc3b800b73c7799032aecfde53425571d78df
Brandon <brandonbgrayson@gmail.com>
2021-06-27 22:19:06 -0400

    added a mat icon home component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
     // imports array
-    imports: [ MatSliderModule, MatButtonModule ],
-    exports: [ MatSliderModule, MatButtonModule ]
+    imports: [ MatSliderModule, MatButtonModule, MatIconModule ],
+    exports: [ MatSliderModule, MatButtonModule, MatIconModule ]
 })
 // a class to manage material components
 export class MaterialModule {
