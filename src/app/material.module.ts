a3a343dec5096f2c3ac7ff952789e3ffe9222185
Brandon <brandonbgrayson@gmail.com>
2021-06-30 23:32:58 -0400

    added email and password field to sign up component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from "@angular/material/icon";
-import { MatFormField } from "@angular/material/form-field";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
     // imports array
-    imports: [ MatSliderModule, MatButtonModule, MatIconModule, MatFormField, MatInputModule],
-    exports: [ MatSliderModule, MatButtonModule, MatIconModule, MatFormField, MatInputModule]
+    imports: [ MatSliderModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+    exports: [ MatSliderModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
 // a class to manage material components
 export class MaterialModule {
